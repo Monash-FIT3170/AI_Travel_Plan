394a12d3719a130011eadb7b0eb1bea731ed226d
cooi0004 <cooi0004@student.monash.edu>
2023-04-22 19:16:43 +1000

    docs: Define data structure for requests and responses

--- server/src/ApplicationTypes.ts
+++ server/src/ApplicationTypes.ts
@@ -1,23 +1,24 @@
 interface Daily_Itinerary {
     day: number
     date: Date
-    events: [Event]
+    events: Event[]
 }
-interface Event1 {
+interface Event {
     name: string
-    address: string | null
-    description: string | null
-    startTime: Date | null
-    endTime: Date | null
-    cost: number | null
+    address: string
+    description?: string
+    startTime: Date
+    endTime: Date
+    cost?: number
+    chatResponse?: string
 }
 interface Travel_Itinerary {
     startDate: Date
     endDate: Date
-    schedule: [Daily_Itinerary]
+    schedule?: Daily_Itinerary[]
 }
 interface Chat_Message {
     message: string
     chatHistory: Travel_Itinerary | String
 }
-export { Daily_Itinerary, Event1, Travel_Itinerary, Chat_Message }
+export { Daily_Itinerary, Event, Travel_Itinerary, Chat_Message }
