e9e2716655faf99e8a84da258666401a0e1e73f2
Luigi Cortez <lcor0011@student.monash.edu>
2023-05-03 13:27:17 +1000

    Added basic layouts for manual itinerary input. No styling/CSS

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
 import Template from "./components/Template";
+import EditButton from "./components/EditButton";
+import NewDestination from "./components/NewDestination";
 function App() {
   return (
     <div>
       <Template></Template>
+      <EditButton />
+      <NewDestination />
+      {/* <AddButton /> */}
     </div>
   );
 }
