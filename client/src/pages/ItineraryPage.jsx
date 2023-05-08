019d99de51b2ecb4919ad41657ab5cf6245c118b
cooi0004 <cooi0004@student.monash.edu>
2023-05-08 23:37:49 +1000

    feat: add itinerary page

---
+++ client/src/pages/ItineraryPage.jsx
@@ -0,0 +1,35 @@
+import React, {useEffect } from "react";
+
+import {mockTravel_Itinerary1}  from "../MockItinerary";
+import {MyForm2}  from "../components/StorageDailyItineraryGeneral";
+import EditButton  from "../components/EditButton";
+import  AddButton  from "../components/AddButton";
+import DeleteButton  from "../components/DeleteButton";
+import DeleteAlert from "../components/DeleteAlert";
+import  NewDestination  from "../components/NewDestination";
+import ItinerarySumarryButton from "../components/ItinerarySummaryButton";
+
+export function ItineraryPage(){
+  useEffect(() => {
+    // Store the mock data in local storage when the page is loaded
+    const itineraryKey = "travelItinerary";
+    const storedItinerary = localStorage.getItem(itineraryKey);
+
+    if (!storedItinerary) {
+        console.log(mockTravel_Itinerary1)
+      localStorage.setItem(itineraryKey, JSON.stringify(mockTravel_Itinerary1));
+    }
+  }, []);
+return (
+<div>
+      <ItinerarySumarryButton />
+       <AddButton />
+       <EditButton />
+        <NewDestination />
+      <DeleteButton></DeleteButton>
+      <DeleteAlert></DeleteAlert>
+      <MyForm2 />
+
+    </div>)
+
+}
