86f9d45fae9e5002fc5e0a552ede067e61491345
arenk <aker0015@student.monash.edu>
2023-05-09 15:09:36 +1000

    feat: able to provide chat history to request

--- server/src/services/ArenOpenAIService.ts
+++ server/src/services/ArenOpenAIService.ts
@@ -4,23 +4,18 @@ dotenv.config()
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
-console.log(process.env.OPENAI_API_KEY)
 const openai = new OpenAIApi(configuration);
 
+// current conceptual structure of a conversation back and forth.
 interface History_Item {
     prompt: string,
     reply: string
 }
 
-/**
- * @fix this request is hard coded, need to change depending on the user input
- * @param message 
- * @param history history of messages from chatgpt
- * @returns 
- */ 
-//TODO: refactor to accept ChatHistory instead of History_Item
-export async function arenSendResponse(message: string, history: History_Item[]) {
 
+//TODO: refactor to accept ChatHistory instead of History_Item
+export async function arenSendResponse(message: string, history: any[]) {
+    console.log("jumped in the function")
     //array of messages to be sent to chatgpt
     let messages: ChatCompletionRequestMessage[] = []
 
@@ -31,14 +26,19 @@ export async function arenSendResponse(message: string, history: History_Item[])
     }
     messages.push(message1)
 
-    // provide context to ChatGPT
+    let completeChat: string = ""
     for (let i = 0; i < history.length; i++) {
-        let tempMessage: ChatCompletionRequestMessage = {
-            role: "system", 
-            content: `Some context: user asked: " ${history[i].prompt}" and you replied: "${history[i].reply}"`
-        }
-        messages.push(tempMessage)
+        completeChat += history[i].prompt + "\n" + history[i].reply + "\n"
     }
+    completeChat += message 
+
+    // provide context to ChatGPT
+    let tempMessage: ChatCompletionRequestMessage = {
+        role: "user", 
+        content: completeChat
+    }
+    messages.push(tempMessage)
+
 
     // provide chat-bot with user request
     const message2: ChatCompletionRequestMessage = {
@@ -51,18 +51,18 @@ export async function arenSendResponse(message: string, history: History_Item[])
     // specify format of the response
     const message3: ChatCompletionRequestMessage = {
         role: "system",
-        content: "return a response in {places: [place:str, date:date, cost:number, description:str, address;str, chatResponse?: string ]} json format with no additional text. Provide the agent response in chatResponse field",
+        content: "return your full response in {places: [place:str, date:date, cost:number, description:str, address;str], chatResponse: string } json format with no additional text. Provide the agent response in the specified chatResponse field where the agent response is a summary of the places you've suggested. make sure you don't suggest places again that are given in earlier messages. If recommending multiple places, separate the places into different items in the array of places, but make sure there is only ever one chatresponse. Also compile all suggestions into the places array",
     }
     messages.push(message3)
 
-
+    console.log(messages)
     const completion = await openai.createChatCompletion(
         {
             "model": "gpt-3.5-turbo",
             "messages": messages
         }
     )
-    //ChatHistory.add
-    return await completion.data.choices[0].message?.content
+    console.log(completion.data.choices[0].message?.content)
+    //return await completion.data.choices[0].message?.content
 
 }
