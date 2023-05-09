6221933ba939e74f665624463c905c67a3cac168
Yu Xuan Yio <yioyuxuan@gmail.com>
2023-05-09 21:49:18 +1000

    feat: edited appbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ChatPage } from "./pages/ChatPage";
 import { ItineraryPage } from "./pages/ItineraryPage";
 import ExamplePage from "./pages/ExamplePage";
