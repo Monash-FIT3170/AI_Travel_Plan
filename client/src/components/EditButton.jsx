850cce1307a20388d6e322d3e00c0baf9f9576c3
dnav0002 <dnav0002@student.monash.edu>
2023-05-15 06:01:02 +1000

    feat: date type edit support and parsing

--- client/src/components/EditButton.jsx
+++ client/src/components/EditButton.jsx
@@ -1,103 +1,155 @@
 import "../style.css";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { useLocalStorage } from "./LocalStorageGeneric";
 
-function EditButton() { // Initialise with invisible popup and empty values
-    const [isPopupVisible, setPopupVisibility] = useState(false);
-    
-    const [itinerary, setItinerary, updateValueInLocalStorage] = useLocalStorage("dailyItinerary", "");
+function EditButton() {
+	// Initialise with invisible popup and empty values
+	const [isPopupVisible, setPopupVisibility] = useState(false);
 
-    const [eventName, setEventName] = useState();
-    const [scheduleIndex, setScheduleIndex] = useState();
-    const [eventsIndex, setEventsIndex] = useState();
+	// Initialise the daily itinerary
+	const [itinerary, setItinerary, updateValueInLocalStorage] = useLocalStorage(
+		"dailyItinerary",
+		""
+	);
 
-    const exampleEventName = 'Meiji Jingu Shrine, Tokyo';
+	// Initialise the event name, schedule index, and events index as states
+	const [eventName, setEventName] = useState();
+	const [scheduleIndex, setScheduleIndex] = useState();
+	const [eventsIndex, setEventsIndex] = useState();
 
+	// Test input for editing an event
+	// Update this to automatically take the input for a specific event
+	const exampleEventName = "Meiji Jingu Shrine, Tokyo";
 
-    const findEvent = (itinerary) => {
-        for (let i = 0; i < Object.keys(itinerary.schedule).length; i++) {
-            for (let j = 0; j < Object.keys(itinerary.schedule[i].events).length; j++) {
-                let currentName = itinerary.schedule[i].events[j].name;
-                if (currentName == exampleEventName) {
-                    let currentEvent = itinerary.schedule[i].events[j];
-                    
-                    // console.log("in schedule location: " + i + " and events location: " + j);
-                    let eventName = currentEvent.name;
-                    setDestination(eventName);
-                    setEventName(eventName);
-                    setScheduleIndex(i);
-                    setEventsIndex(j);
-                }
-            }
-        }
-    }
+	function parseDateFromISO(dateString) {
+		const year = dateString.slice(0, 4);
+		const month = dateString.slice(5, 7);
+		const day = dateString.slice(8, 10);
+		const hours = dateString.slice(11, 13);
+		const minutes = dateString.slice(14, 16);
+		const seconds = dateString.slice(17, 19);
+		const milliseconds = dateString.slice(20, 23);
 
-    console.log(itinerary.schedule[1].events);
-    // Pass existing information into useState
-    const [destinationVal, setDestination] = useState(eventName);
-    const [dateVal, setDate] = useState(new Date('2023-05-27'));
-    const [timeVal, setTime] = useState();
+		return new Date(
+			Date.UTC(year, month - 1, day, hours, minutes, seconds, milliseconds)
+		);
+	}
 
+	function parseDateToISO(date) {
+		const year = date.getUTCFullYear();
+		const month = String(date.getUTCMonth() + 1).padStart(2, "0");
+		const day = String(date.getUTCDate()).padStart(2, "0");
+		const hours = String(date.getUTCHours()).padStart(2, "0");
+		const minutes = String(date.getUTCMinutes()).padStart(2, "0");
+		const seconds = String(date.getUTCSeconds()).padStart(2, "0");
+		const milliseconds = String(date.getUTCMilliseconds()).padStart(3, "0");
 
-    // Updating the value when the input is changed
-    const handleUpdate = (event, inputNum) => {
-        const value = event.target.value;
-        const fields = {
-            1: setDestination,
-            2: setDate,
-            3: setTime
-        };
-        const setInput = fields[inputNum];
-        setInput(value);
-    };
+		return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}.${milliseconds}Z`;
+	}
 
-    // Save the values once user clicks save
-    const handleSave = () => { // Use destinationVal, dateVal, timeVal to store where needed
-        itinerary.schedule[scheduleIndex].events[eventsIndex].name = destinationVal;
-        console.log(itinerary);
-        setPopupVisibility(false);
-    };
+	const findEvent = (itinerary) => {
+		for (let i = 0; i < Object.keys(itinerary.schedule).length; i++) {
+			for (
+				let j = 0;
+				j < Object.keys(itinerary.schedule[i].events).length;
+				j++
+			) {
+				let currentName = itinerary.schedule[i].events[j].name;
+				if (currentName == exampleEventName) {
+					let currentEvent = itinerary.schedule[i].events[j];
 
-    // Showing the popup with input fields and save button
-    return (
-        <div>
-            <button onClick={
-                () => {
-                    setPopupVisibility(true);
-                    findEvent(itinerary);}
-            }>Edit destination</button>
-            {
-            isPopupVisible && (
-                <div className="overlay">
-                    <div className="popup">
-                        <h2>Edit destination</h2>
-                        <label htmlFor="inputDestination">Destination:</label>
-                        <input id="inputDestination" type="text"
-                            value={destinationVal}
-                            onChange={
-                                (e) => handleUpdate(e, 1)
-                            }/>
-                        <br></br>
-                        <label htmlFor="inputDate">Date:</label>
-                        <input id="inputDate" type="date"
-                            value={dateVal.toISOString().slice(0, 10)}
-                            onChange={
-                                (e) => handleUpdate(e, 2)
-                            }/>
-                        <br></br>
-                        <label htmlFor="inputTime">Time:</label>
-                        <input id="inputTime" type="time"
-                            value={timeVal}
-                            onChange={
-                                (e) => handleUpdate(e, 3)
-                            }/>
-                        <br></br>
-                        <button onClick={handleSave}>Save</button>
-                    </div>
-                </div>
-            )
-        } </div>
-    );
+					// console.log("in schedule location: " + i + " and events location: " + j);
+					let eventName = currentEvent.name;
+					setDestination(eventName);
+					setDate(parseDateFromISO(currentEvent.startTime));
+					setEventName(eventName);
+					setScheduleIndex(i);
+					setEventsIndex(j);
+				}
+			}
+		}
+	};
+
+	// Pass existing information into useState
+	const [destinationVal, setDestination] = useState(eventName);
+	const [dateVal, setDate] = useState(null);
+	const [timeVal, setTime] = useState();
+
+	// Updating the value when the input is changed
+	const handleUpdate = (event, inputNum) => {
+		const value = event.target.value;
+		const fields = {
+			1: setDestination,
+			2: (val) => {
+				if (val) {
+					const date = parseDateFromISO(val);
+					setDate(date);
+				} else {
+					setDate(null);
+				}
+			},
+			3: setTime,
+		};
+		const setInput = fields[inputNum];
+		setInput(value);
+	};
+
+	// Save the values once user clicks save
+	const handleSave = () => {
+		// Use destinationVal, dateVal, timeVal to store where needed
+		itinerary.schedule[scheduleIndex].events[eventsIndex].name = destinationVal;
+		itinerary.schedule[scheduleIndex].events[eventsIndex].startTime =
+			parseDateToISO(dateVal);
+		console.log(itinerary);
+		updateValueInLocalStorage(itinerary);
+		setPopupVisibility(false);
+	};
+
+	// Showing the popup with input fields and save button
+	return (
+		<div>
+			<button
+				onClick={() => {
+					setPopupVisibility(true);
+					findEvent(itinerary);
+				}}
+			>
+				Edit destination
+			</button>
+			{isPopupVisible && (
+				<div className="overlay">
+					<div className="popup">
+						<h2>Edit destination</h2>
+						<label htmlFor="inputDestination">Destination:</label>
+						<input
+							id="inputDestination"
+							type="text"
+							value={destinationVal}
+							onChange={(e) => handleUpdate(e, 1)}
+						/>
+						<br></br>
+						<label htmlFor="inputDate">Date:</label>
+						<input
+							id="inputDate"
+							type="date"
+							value={dateVal.toISOString().slice(0, 10)}
+							onChange={(e) => handleUpdate(e, 2)}
+						/>
+						<br></br>
+						<label htmlFor="inputTime">Time:</label>
+						<input
+							id="inputTime"
+							type="time"
+							value={timeVal}
+							onChange={(e) => handleUpdate(e, 3)}
+						/>
+						<br></br>
+						<button onClick={handleSave}>Save</button>
+					</div>
+				</div>
+			)}{" "}
+		</div>
+	);
 }
 
 export default EditButton;
