422dd7e2ef8e9ed5e78b536602abe2b278276e56
Ctha0006 <ctha0006@student.monash.edu>
2023-05-15 04:58:35 +1000

    feat: handleDelete method deletes an event from the local storage itinerary

--- client/src/components/DeleteButton.jsx
+++ client/src/components/DeleteButton.jsx
@@ -1,24 +1,30 @@
 import "../style.css";
 import React, {useState} from "react";
+import { useLocalStorage } from "./LocalStorageGeneric";
+import { mockDaily_Itinerary1, mockEvent1 } from "./MockData";
 
 function DeleteButton() {
     const [isPopupVisible, setPopupVisibility] = useState(false);
-
-    // Pass existing information into useState
-    const [destinationVal] = useState("Test");
-    const [dateVal] = useState();
-    const [timeVal] = useState();
+    const [itinerary, setItinerary, updateValueInLocalStorage] = useLocalStorage("dailyItinerary", mockDaily_Itinerary1);
 
     // Delete the event from history once confirmed
-    const handleDelete = () => {
+    const handleDelete = (eventName) => {
+        console.log(eventName);
+        // get the itinerary item, find the matching event to delete, edit it, then set the itinerary again
+        const updatedEvents = itinerary.schedule[0].events.filter((event) => event.name !== eventName);
+        console.log(updatedEvents);
+        const updatedItinerary = itinerary;
+        updatedItinerary.schedule[0].events = updatedEvents;
+        console.log(updatedItinerary);
+        updateValueInLocalStorage(updatedItinerary);
+
         setPopupVisibility(false);
         var x = document.getElementById("deletedAlert");
         x.className = "show";
         setTimeout(function(){ x.className = x.className.replace("show", ""); }, 2000);
-        
     };
     
-    // Delete the event from history once confirmed
+    // Cancel deleting pop up
     const handleCancel = () => {
         setPopupVisibility(false);
     };
@@ -34,10 +40,8 @@ function DeleteButton() {
                 <div className="overlay">
                     <div className="popup">
                         <h2>Delete destination</h2>
-                        <button id="cancelButton" onClick={
-                            () => setPopupVisibility(false)
-                        }>Cancel</button>
-                        <button id="deleteButton" onClick={handleDelete}>Delete</button>
+                        <button id="cancelButton" onClick={handleCancel}>Cancel</button>
+                        <button id="deleteButton" onClick={() => handleDelete(mockEvent1.name)}>Delete</button>
                     </div>
                 </div>
             )
