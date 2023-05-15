274ecd7553e37782cad474de76f32a2f768cf4b4
cooi0004 <cooi0004@student.monash.edu>
2023-05-16 00:33:25 +1000

    fix: revert chatpage to previous design

--- client/src/pages/ChatPage.jsx
+++ client/src/pages/ChatPage.jsx
@@ -1,38 +1,26 @@
-import Chatbox from '../components/Chatbox';
-import { useState } from 'react';
-import axios from 'axios';
-import {mockTravel_Itinerary1} from '../MockItinerary';
-import Background from '../components/Background';
-import BackgroundImage from '../components/BackgroundImage';
+import ChatBox from '../components/chatbox/ChatBox';
+import Background from "../components/Background";
+import BackgroundImage from "../components/BackgroundImage";
+import Grid from '@mui/material/Grid';
+import { ItineraryTimeLine } from "../components/ItineraryTimeLine"
+import { mockTravel_Itinerary1 } from "../MockItinerary";
 
-let reply = [];
-
-export function  ChatPage(){
-const [message, setMessages] = useState({prompt: "", reply:""});
-
-const itinerary = {};
-
-const handleChange = (e) => {
-  setMessages({...message,prompt: e.target.value});
-
-}
-const send = ()=> {
-  reply.push(message)
-  console.log(reply)  
-  axios.post('http://127.0.0.1:4000/api/chatMessage', {prompt: message.prompt, travelItinerary: {}, chatHistory: reply}).then((response) => {
-  // reply = {text: response.data.text, type: response.data.type, itinerary: response.data.itinerary}
-  console.log(response)
-  setMessages((message)=> (response.data.message.chatResponse?{...message, reply: message.show+'\n'+ response.data.message.chatResponse}:{...message, reply: message.show+'\n'+ response.data.message})) })
-}
-    return (
-        <div>
-          <div>{message.prompt}</div>
-          <div>{message.reply}</div>
-
-          
-
-          <input onChange= {handleChange}></input>
-          <button onClick = {send}>Send</button>
-        </div>
-    )
+export function ChatPage() {
+  return (
+    <>
+      <BackgroundImage />
+      <Background>
+      <Grid container>
+                  <Grid item xs={6}>
+                  <ChatBox></ChatBox>
+                </Grid>
+                <Grid item xs={6} style={{ height: '100vh', overflowY: 'auto' }}>
+    <ItineraryTimeLine travelItinerary={mockTravel_Itinerary1}/>
+                </Grid>
+        
+            </Grid>
+        
+      </Background>
+    </>
+  );
 }
