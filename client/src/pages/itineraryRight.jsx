3cad5be22bd631aeaac6b31c224ece113cfe5521
Kai Lyne Loi <kloi0002@student.monash.edu>
2023-05-16 12:50:50 +1000

    feat: create a "Add new location" button

--- client/src/pages/itineraryRight.jsx
+++ client/src/pages/itineraryRight.jsx
@@ -5,24 +5,28 @@ import { EventCardView } from "../components/EventCardView"
 import { ItineraryTimeLine } from "../components/ItineraryTimeLine"
 import Grid from '@mui/material/Grid';
 import { mockTravel_Itinerary1 } from "../MockItinerary";
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
 
-export function ItineraryRight(){
+export function ItineraryRight() {
     return (
-        <div>
-            <BackgroundImage/>
-            <Background>
-            <Grid container>
-                  <Grid item xs={6}>
-                Follow figma for the components here
-                </Grid>
-                <Grid item xs={6} style={{ height: '100vh', overflowY: 'auto' }}>
-    <ItineraryTimeLine travelItinerary={mockTravel_Itinerary1}/>
-                </Grid>
-        
+      <div style={{ position: 'relative' }}>
+        <BackgroundImage />
+        <Background>
+          <Grid container>
+            <Grid item xs={6}>
+              Follow figma for the components here
             </Grid>
-            
-            </Background>
-        </div>
-
-    )
-}
+            <Grid item xs={6} style={{ height: '100vh', overflowY: 'auto' }}>
+              <ItineraryTimeLine travelItinerary={mockTravel_Itinerary1} />
+              <div style={{ position: 'fixed', bottom: '20px', right: '50px' }}>
+                <Button variant="contained" endIcon={<AddIcon />}>
+                  ADD NEW LOCATION
+                </Button>
+              </div>
+            </Grid>
+          </Grid>
+        </Background>
+      </div>
+    );
+  }
