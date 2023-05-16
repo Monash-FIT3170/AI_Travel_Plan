a55e4b2274a8bae6548a46d3633f6be331f26999
Yu Xuan Yio <yioyuxuan@gmail.com>
2023-05-17 03:06:18 +1000

    fix: removed localisation in date & time

--- client/src/components/EventCardView.jsx
+++ client/src/components/EventCardView.jsx
@@ -16,191 +16,198 @@ import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { TimePicker } from "@mui/x-date-pickers/TimePicker";
 import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 export function EventCardView({ event, itinerary, setItinerary }) {
-	const [open, setOpen] = useState(false);
-	const [deleteOpen, setDeleteOpen] = useState(false);
-	const [name, setName] = useState(event.name);
-	const [description, setDescription] = useState(event.description);
-	const [cost, setCost] = useState(event.cost);
-	const [location, setLocation] = useState(event.location);
-	const [chatResponse, setResponse] = useState(event.chatResponse);
-	const [date, setDate] = useState(dayjs(event.startTime));
-	const [time, setTime] = useState(dayjs(event.startTime));
-	const [errors, setErrors] = useState({ name: "", date: "", time: "" });
+  const [open, setOpen] = useState(false);
+  const [deleteOpen, setDeleteOpen] = useState(false);
+  const [name, setName] = useState(event.name);
+  const [description, setDescription] = useState(event.description);
+  const [cost, setCost] = useState(event.cost);
+  const [location, setLocation] = useState(event.location);
+  const [chatResponse, setResponse] = useState(event.chatResponse);
+  const [date, setDate] = useState(dayjs(event.startTime));
+  const [time, setTime] = useState(dayjs(event.startTime));
+  const [errors, setErrors] = useState({ name: "", date: "", time: "" });
+  let specificTimezone = "America/New_York";
 
-	const handleClickOpen = () => {
-		setOpen(true);
-	};
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-	const handleClose = () => {
-		setOpen(false);
-		setErrors({ name: "", date: "", time: "" });
-	};
+  const handleClose = () => {
+    setOpen(false);
+    setErrors({ name: "", date: "", time: "" });
+  };
 
-	const handleDeleteOpen = () => {
-		setDeleteOpen(true);
-	};
+  const handleDeleteOpen = () => {
+    setDeleteOpen(true);
+  };
 
-	const handleDeleteClose = () => {
-		setDeleteOpen(false);
-	};
+  const handleDeleteClose = () => {
+    setDeleteOpen(false);
+  };
 
-	const handleDeleteConfirm = () => {
-		// Remove the event from the itinerary
-		const updatedItinerary = {
-			...itinerary,
-			schedule: itinerary.schedule.map((day) => ({
-				...day,
-				events: day.events.filter((e) => e.name !== event.name),
-			})),
-		};
+  const handleDeleteConfirm = () => {
+    // Remove the event from the itinerary
+    const updatedItinerary = {
+      ...itinerary,
+      schedule: itinerary.schedule.map((day) => ({
+        ...day,
+        events: day.events.filter((e) => e.name !== event.name),
+      })),
+    };
 
-		// Update the itinerary
-		setItinerary(updatedItinerary);
+    // Update the itinerary
+    setItinerary(updatedItinerary);
 
-		// Close the delete confirmation dialog
-		setDeleteOpen(false);
-	};
+    // Close the delete confirmation dialog
+    setDeleteOpen(false);
+  };
 
-	useEffect(() => {
-		let nameError = "";
-		let dateError = "";
-		let timeError = "";
+  useEffect(() => {
+    let nameError = "";
+    let dateError = "";
+    let timeError = "";
 
-		if (!name.trim()) {
-			nameError = "Name is required";
-		}
-		if (!date.isValid()) {
-			dateError = "Date is not valid";
-		}
-		if (!time.isValid()) {
-			timeError = "Time is not valid";
-		}
+    if (!name.trim()) {
+      nameError = "Name is required";
+    }
+    if (!date.isValid()) {
+      dateError = "Date is not valid";
+    }
+    if (!time.isValid()) {
+      timeError = "Time is not valid";
+    }
 
-		setErrors({ name: nameError, date: dateError, time: timeError });
-	}, [name, date, time]);
+    setErrors({ name: nameError, date: dateError, time: timeError });
+  }, [name, date, time]);
 
-	const handleSave = () => {
-		if (errors.name || errors.date || errors.time) {
-			alert("Please enter valid inputs before saving.");
-			return;
-		}
+  const handleSave = () => {
+    if (errors.name || errors.date || errors.time) {
+      alert("Please enter valid inputs before saving.");
+      return;
+    }
 
-		const updatedItinerary = {
-			...itinerary,
-			schedule: itinerary.schedule.map((day) => {
-				// Filter out the event with a matching name from the original events array
-				const updatedEvents = day.events.filter((e) => e.name !== event.name);
+    const updatedItinerary = {
+      ...itinerary,
+      schedule: itinerary.schedule.map((day) => {
+        // Filter out the event with a matching name from the original events array
+        const updatedEvents = day.events.filter((e) => e.name !== event.name);
 
-				// Check if the current day's date matches the selected date
-				if (dayjs(day.date).isSame(date, "day")) {
-					// Create a new event object with updated values
-					const newEvent = {
-						chatResponse,
-						cost,
-						description,
-						endTime: date
-							.add(
-								dayjs(event.endTime).diff(dayjs(event.startTime), "hour"),
-								"hour"
-							)
-							.toDate(),
-						location,
-						name,
-						startTime: date.toDate(),
-					};
+        // Check if the current day's date matches the selected date
+        if (dayjs(day.date).isSame(date, "day")) {
+          // Create a new event object with updated values
+          const newEvent = {
+            chatResponse,
+            cost,
+            description,
+            endTime: date
+              .add(
+                dayjs(event.endTime).diff(dayjs(event.startTime), "hour"),
+                "hour",
+              )
+              .toDate(),
+            location,
+            name,
+            startTime: date.toDate(),
+          };
 
-					// Add the new event to the end of the array
-					updatedEvents.push(newEvent);
-				}
+          // Add the new event to the end of the array
+          updatedEvents.push(newEvent);
+        }
 
-				return {
-					...day,
-					events: updatedEvents,
-				};
-			}),
-		};
+        return {
+          ...day,
+          events: updatedEvents,
+        };
+      }),
+    };
 
-		setItinerary(updatedItinerary);
-		setOpen(false);
-	};
+    setItinerary(updatedItinerary);
+    setOpen(false);
+  };
 
-	return (
-		<Box display="flex" justifyContent="stretch" width="100%">
-			<Card variant="outlined" style={{ width: "100%" }}>
-				<CardHeader
-					avatar={<PlaceIcon />}
-					title={event.name}
-					subheader={
-						"DURATION: " +
-						Math.floor((event.endTime - event.startTime) / (1000 * 60 * 60)) +
-						" HRS"
-					}
-				/>
-				<CardContent>{event.description}</CardContent>
-				<CardActions>
-					<Button size="small" onClick={handleDeleteOpen}>
-						Delete
-					</Button>
-					<Button size="small" onClick={handleClickOpen}>
-						Edit
-					</Button>
-				</CardActions>
-			</Card>
+  return (
+    <Box display="flex" justifyContent="stretch" width="100%">
+      <Card variant="outlined" style={{ width: "100%" }}>
+        <CardHeader
+          avatar={<PlaceIcon />}
+          title={event.name}
+          subheader={
+            "DURATION: " +
+            Math.floor((event.endTime - event.startTime) / (1000 * 60 * 60)) +
+            " HRS"
+          }
+        />
+        <CardContent>{event.description}</CardContent>
+        <CardActions>
+          <Button size="small" onClick={handleDeleteOpen}>
+            Delete
+          </Button>
+          <Button size="small" onClick={handleClickOpen}>
+            Edit
+          </Button>
+        </CardActions>
+      </Card>
 
-			<Dialog open={deleteOpen} onClose={handleDeleteClose}>
-				<DialogTitle>This action cannot be undon</DialogTitle>
-				<DialogContent>
-					Deleting this item will remove it from the itinerary and cannot be
-					undone.
-				</DialogContent>
-				<DialogActions>
-					<Button onClick={handleDeleteClose}>Cancel</Button>
-					<Button onClick={handleDeleteConfirm}>Confirm</Button>
-				</DialogActions>
-			</Dialog>
+      <Dialog open={deleteOpen} onClose={handleDeleteClose}>
+        <DialogTitle>This action cannot be undon</DialogTitle>
+        <DialogContent>
+          Deleting this item will remove it from the itinerary and cannot be
+          undone.
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleDeleteClose}>Cancel</Button>
+          <Button onClick={handleDeleteConfirm}>Confirm</Button>
+        </DialogActions>
+      </Dialog>
 
-			<Dialog open={open} onClose={handleClose} maxWidth="xs">
-				<DialogTitle>Edit Event</DialogTitle>
-				<DialogContent>
-					<TextField
-						autoFocus
-						margin="dense"
-						label="Name"
-						type="text"
-						fullWidth
-						value={name}
-						onChange={(e) => setName(e.target.value)}
-						error={Boolean(errors.name)}
-						helperText={errors.name}
-					/>
-					<LocalizationProvider dateAdapter={AdapterDayjs}>
-						<Box width={"100%"} mt={2}>
-							<DatePicker
-								label="Date"
-								value={date}
-								onChange={(newDate) => setDate(newDate)}
-								error={Boolean(errors.date)}
-								helperText={errors.date}
-							/>
-						</Box>
-						<Box width={"100%"} mt={2}>
-							<TimePicker
-								label="Time"
-								value={time}
-								onChange={(newTime) => setTime(newTime)}
-								error={Boolean(errors.time)}
-								helperText={errors.time}
-							/>
-						</Box>
-					</LocalizationProvider>
-				</DialogContent>
-				<DialogActions>
-					<Button onClick={handleClose}>Cancel</Button>
-					<Button onClick={handleSave}>Save</Button>
-				</DialogActions>
-			</Dialog>
-		</Box>
-	);
+      <Dialog open={open} onClose={handleClose} maxWidth="xs">
+        <DialogTitle>Edit Event</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            label="Name"
+            type="text"
+            fullWidth
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            error={Boolean(errors.name)}
+            helperText={errors.name}
+          />
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <Box width={"100%"} mt={2}>
+              <DatePicker
+                label="Date"
+                // Removing localisation for now.
+                value={date.tz(specificTimezone)}
+                onChange={(newDate) => setDate(newDate)}
+                error={Boolean(errors.date)}
+                helperText={errors.date}
+              />
+            </Box>
+            <Box width={"100%"} mt={2}>
+              <TimePicker
+                label="Time"
+                // Removing localisation for now.
+                value={time.tz(specificTimezone)}
+                onChange={(newTime) => setTime(newTime)}
+                error={Boolean(errors.time)}
+                helperText={errors.time}
+              />
+            </Box>
+          </LocalizationProvider>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleSave}>Save</Button>
+        </DialogActions>
+      </Dialog>
+    </Box>
+  );
 }
