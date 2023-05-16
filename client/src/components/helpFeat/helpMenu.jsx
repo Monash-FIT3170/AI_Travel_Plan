5925e38cd74702be4510fa79e136272a3b805393
github-devireddyprasanth22 <github-dpkrdrkr@gmail.com>
2023-05-16 11:00:22 +1000

    Help page set up

---
+++ client/src/components/helpFeat/helpMenu.jsx
@@ -0,0 +1,11 @@
+import Accordion from "@mui/material/Accordion";
+import AccordionActions from "@mui/material/AccordionActions";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import React, { useState } from "react";
+
+/**
+ * Contains the entire code for the help accordian menu
+ * @returns
+ */
+export default function helpMenu() {}
--- client/src/pages/ExamplePage.jsx
+++ client/src/pages/ExamplePage.jsx
@@ -1,25 +1,21 @@
-/**
- * This file is an example of how a page looks in the application
- * Consider a page as being a single page of the application
- * for example, you may be redirected from the HOME page to a LOG IN page to log into your account
- * Each page should contain all the relevant components specific to that page
- *
- * In terms of react components, a page is also just a react component.
- */
-
 import React from "react";
 import ExampleBox from "../components/examples/ExampleBox";
 import Background from "../components/Background";
 import BackgroundImage from "../components/BackgroundImage";
+import Typography from "@mui/material/Typography";
+import helpMenu from "..components/helpFeat/helpMenu";
 
 export default function ExamplePage() {
   return (
     <>
       <BackgroundImage />
       <Background>
-        <ExampleBox />
-        <ExampleBox />
-        <ExampleBox />
+        <Typography
+          variant="h3"
+          style={{ textAlign: "center", color: "white", marginTop: "100px" }}
+        >
+          We are here to help{" "}
+        </Typography>
       </Background>
     </>
   );
