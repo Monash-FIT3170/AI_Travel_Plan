7cac6a22a796f9e1bf0d4dad9c7ead36450a8c18
github-devireddyprasanth22 <github-dpkrdrkr@gmail.com>
2023-05-18 21:26:58 +1000

    HelpPage: Styling added with suitable reponses

--- client/src/components/help/HelpMenu.jsx
+++ client/src/components/help/HelpMenu.jsx
@@ -13,59 +13,84 @@ import "./HelpMenuStyles.css";
 export default function HelpMenu() {
   return (
     <div className="help-menu">
-      <Accordion>
+      <Accordion className="accordion">
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel1a-content"
           id="panel1a-header"
+          className="accordion-summary"
         >
           <Typography>How do I navigate this page?</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>Response 1</Typography>
+          <Typography>
+            The top navigation menu allows you to move between the important
+            pages. This includes the Travel Planner, where you can chat with the
+            AI chatbot, the Itinerary, where you can view the itinerary and
+            other information, and Help, where you can understand what you can
+            do in order to maximise your experience using this website.{" "}
+          </Typography>
         </AccordionDetails>
       </Accordion>
-      <Accordion>
+      <Accordion className="accordion">
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel2a-content"
           id="panel2a-header"
+          className="accordion-summary"
         >
           <Typography>How do I add items to the itinerary?</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>idk</Typography>
+          <Typography>
+            You can add items to the itinerary by simply talking with our AI
+            travel planer, who will add destinations that you would like to
+            visit in the itinerary.
+          </Typography>
         </AccordionDetails>
       </Accordion>
-      <Accordion>
+      <Accordion className="accordion">
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel2a-content"
           id="panel2a-header"
+          className="accordion-summary"
         >
-          <Typography>Is there a way for me to export my itinerary</Typography>
+          <Typography>
+            Is there a way for me to export/download my itinerary
+          </Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>Response 2</Typography>
+          <Typography>
+            Yes, you can download your itinerary. Head to the Itinerary tab, and
+            under extra information, you should be able to see a button that
+            allows you to download the itinerary{" "}
+          </Typography>
         </AccordionDetails>
       </Accordion>
-      <Accordion>
+      <Accordion className="accordion">
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel2a-content"
           id="panel2a-header"
+          className="accordion-summary"
         >
           <Typography>Is my itinerary information up to date?</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>Response 2</Typography>
+          <Typography>
+            Yes, all your itinerary information is up to date, and this includes
+            all the extra information provided such as the forex exchange rate
+            and weather
+          </Typography>
         </AccordionDetails>
       </Accordion>
-      <Accordion>
+      <Accordion className="accordion">
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel2a-content"
           id="panel2a-header"
+          className="accordion-summary"
         >
           <Typography>
             Where can I find emergency contact information about the place I am
@@ -73,7 +98,12 @@ export default function HelpMenu() {
           </Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>Response 2</Typography>
+          <Typography>
+            All emergency information regarding your destination can be found in
+            the Itinerary page, as part of the extra information provided. These
+            emergency contacts include Ambulance, Police, Fire department and
+            more.{" "}
+          </Typography>
         </AccordionDetails>
       </Accordion>
     </div>
