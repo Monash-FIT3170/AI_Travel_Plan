125e1d2c41523f5e5de60dbb3fae841127acf207
arenk <aker0015@student.monash.edu>
2023-05-18 12:29:14 +1000

    refactor: aren+Ethan changes styled correctly

--- client/src/components/chatbox/ChatBox.jsx
+++ client/src/components/chatbox/ChatBox.jsx
@@ -12,7 +12,7 @@ import React, { useState } from "react";
  * Contains the entire code for a chat box area, including text field, message display.
  * @returns
  */
-export default function Chatbox () {
+export default function Chatbox() {
   /**
    * State - inputValue: the value in the text box
    */
@@ -21,13 +21,16 @@ export default function Chatbox () {
   /**
    * State - outboxValue: the output value from the server
    */
-   const [outboxValue, setOutboxValue] = useState("");
+  const [outboxValue, setOutboxValue] = useState("");
 
   /**
    * State - messges: list of messages in this chat
    */
-   const [messages, setMessages] = useState([
-    { text: "Hello, I am your AI Travel Planner. How can I help you today?", sender: "server" },
+  const [messages, setMessages] = useState([
+    {
+      text: "Hello, I am your AI Travel Planner. How can I help you today?",
+      sender: "server",
+    },
   ]);
 
   /**
@@ -36,38 +39,36 @@ export default function Chatbox () {
    * TODO: create new message and add it to the message list
    */
   const handleButtonClick = (event) => {
-    if (inputValue.length <= 0) {
-      // ignore empty string
-      return;
+    if (inputValue.length > 0) {
+      addMessage(inputValue);
+      event.preventDefault();
+
+      // Placeholder message while fetching
+      setOutboxValue("Loading...");
+
+      // Simulating API call, replace with actual server's response
+      setTimeout(() => {
+        const response = "testing";
+
+        // Add user's input message and server's response to the messages state
+        const updatedMessages = [
+          ...messages,
+          { text: inputValue, sender: "user" },
+          { text: response, sender: "server" },
+        ];
+        setMessages(updatedMessages);
+
+        // Clear the input field
+        setInputValue("");
+      });
     }
-    addMessage(inputValue);
-    event.preventDefault();
-
-   // Placeholder message while fetching
-    setOutboxValue("Loading..."); 
-
-    // Simulating API call, replace with actual server's response
-    setTimeout(() => {
-      const response = "testing";
-
-      // Add user's input message and server's response to the messages state
-      const updatedMessages = [
-        ...messages,
-        { text: inputValue, sender: "user" },
-        { text: response, sender: "server" },
-      ];
-      setMessages(updatedMessages);
-
-      // Clear the input field
-      setInputValue("");
-    }); 
   };
 
   /**
    * Allows the TextField to add more characters whenever there's an input
    * @param {*} event => event.target.value contains the input when a new character is added.
    */
-   const handleInputEnter = (event) => {
+  const handleInputEnter = (event) => {
     setInputValue(event.target.value);
   };
 
@@ -75,85 +76,68 @@ export default function Chatbox () {
    * adds a new message to the list of messages
    * @param {String} newMessage new message to add to the message list
    */
-   const addMessage = (newMessage) => {
+  const addMessage = (newMessage) => {
     setMessages([...messages, newMessage]);
-    //just for testing vv
-    //TODO: the messages printed are delayed by 1 message. Why is that?
-    console.log("\nhere is the array contents:");
-    for (let i = 0; i < messages.length; i++) {
-      console.log(messages[i]);
-    }
-    console.log("end of contents\n");
   };
 
-  /** 
+  /**
    * jsx render
    */
   return (
     // Flexbox with 73% fixed height so messages don't overlap on the input text field
-    <div style={{ display: "flex", height: "73vh" }}> 
+    <div style={{ display: "flex", height: "73vh" }}>
       {/* Scrolling div for messages*/}
-      <div style={{ flex: "1", overflowY: "auto", paddingTop: "20px"}}>
+      <div style={{ flex: "1", overflowY: "auto", overflowX: "none", paddingTop: "20px" }}>
         {/* Display each Message */}
         {messages.map((message, index) => (
           <div key={index} style={{ display: "flex" }}>
-            {/* Message from Server */}
-            {message.sender === "server" && (
-              <div style={{ marginRight: "auto", marginBottom: "10px", marginLeft: "15px", width: "70%" }}>
-                <FormControl
-                  as="textarea"
-                  rows={3}
-                  value={message.text}
-                  readOnly
-                  style={{ resize: "none" }}
-                />
-              </div>
-            )}
-            {/* Message from User */}
-            {message.sender === "user" && (
-              <div style={{ marginLeft: "auto", marginBottom: "10px", width: "70%" }}>
-                <FormControl
-                  as="textarea"
-                  rows={3}
-                  value={message.text}
-                  readOnly
-                  style={{ resize: "none" }}
-                />
-              </div>
-            )}
+            <div
+              style={{
+                marginBottom: "10px",
+                marginLeft: "15px",
+                marginRight: "15px",
+                width: "98%",
+              }}
+            >
+              <MessageCard message={message.text} sender={message.sender} />
+            </div>
           </div>
         ))}
       </div>
       {/* Input message text field */}
-      <div style={{ position: "fixed", bottom: "0", left: "0", width: "100%", padding: "10px" }}>
-        <Form onSubmit={handleButtonClick}>
-          <Form.Group style={{ margin: "0", padding: "5px" }}>
-            <Box display="flex" alignItems="center">
-              <TextField
-                type="text"
-                placeholder="Type in your message"
-                value={inputValue}
-                onChange={handleInputEnter}
-                multiline
-                rows={1}
-                style={{ width: "50%", resize: "none" }}
-                variant="outlined"
-                InputProps={{
-                  endAdornment: (
-                    <IconButton
-                      type="submit"
-                      edge="end"
-                      style={{ color: "white", backgroundColor: "black" }}
-                    >
-                      <SendIcon />
-                    </IconButton>
-                  ),
-                }}
-              />
-            </Box>
-          </Form.Group>
-        </Form>
+      <div
+        style={{
+          position: "fixed",
+          bottom: "0",
+          left: "0",
+          width: "100%",
+          padding: "15px",
+        }}
+      >
+        <Box display="flex" alignItems="center">
+          <TextField
+            placeholder="Type in your message"
+            value={inputValue}
+            onChange={handleInputEnter}
+            multiline
+            maxRows="4"
+            minRows="1"
+            style={{ width: "50%", resize: "none" }}
+            variant="outlined"
+            InputProps={{
+              endAdornment: (
+                <IconButton
+                  onClick={handleButtonClick}
+                  edge="end"
+                  style={{ color: "white", backgroundColor: "black" }}
+                >
+                  <SendIcon />
+                </IconButton>
+              ),
+            }}
+          />
+        </Box>
       </div>
     </div>
   );
-};
+}
--- client/src/components/chatbox/MessageCard.jsx
+++ client/src/components/chatbox/MessageCard.jsx
@@ -9,28 +9,44 @@ import { Box, Typography } from "@mui/material";
  */
 export default function MessageCard(props) {
   //determine the side of the screen to appear on
-  const alignStyle = props.sender === "me" ? "flex-end" : "flex-start";
+  //const alignStyle = props.sender === "user" ? "flex-end" : "flex-start";
 
   //determine the background colour
-  const backgroundStyle = props.sender === "me" ? "#DCF8C6" : "#ECEFF1";
+  const backgroundStyle = props.sender === "user" ? "#DCF8C6" : "#ECEFF1";
+
+  let messageStyle = null
+  if (props.sender==="user") {
+    messageStyle = {        
+    width:"max-content", // the size is automatically set to the size of the message inside
+    minWidth: "0px", // minimum size is 0
+    maxWidth: "60%", // maximum size is set based on box size
+    marginLeft: "auto",
+    marginBottom: "1",
+    overflowWrap: "break-word",
+  }
+  } else if (props.sender==="server") {
+    messageStyle = {        
+      width:"max-content", // the size is automatically set to the size of the message inside
+      minWidth: "0px", // minimum size is 0
+      maxWidth: "60%", // maximum size is set based on box size
+      marginRight: "auto",
+      marginBottom: "1",
+      overflowWrap: "break-word",
+    }
+  }
+  
 
   //the message to be displayed
-  const message = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-  Curabitur finibus sed ante et ornare. Morbi vitae fringilla elit, a 
-  scelerisque mauris. Aliquam sodales lacus urna, elementum `;
+  // const message = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+  // Curabitur finibus sed ante et ornare. Morbi vitae fringilla elit, a 
+  // scelerisque mauris. Aliquam sodales lacus urna, elementum `;
 
   return (
     <Box
-      style={{
-        width:"max-content", // the size is automatically set to the size of the message inside
-        minWidth: "0px", // minimum size is 0
-        maxWidth: "60%", // maximum size is set based on box size
-        alignSelf: alignStyle, // to the left or the right? depends on props.sender for now
-        marginBottom: "1",
-      }}
+      style={messageStyle}
     >
       <Box padding={1} borderRadius={4} bgcolor={backgroundStyle}>
-        <Typography variant="body1">{message}</Typography>
+        <Typography variant="body1">{props.message}</Typography>
       </Box>
     </Box>
   );
