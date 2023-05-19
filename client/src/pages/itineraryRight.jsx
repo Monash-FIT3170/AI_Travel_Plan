dba22efeed48d7043dde6306b65109056b4d5bbf
Kai Lyne Loi <kloi0002@student.monash.edu>
2023-05-19 19:53:41 +1000

    wip: able to display add pop up

--- client/src/pages/itineraryRight.jsx
+++ client/src/pages/itineraryRight.jsx
@@ -10,14 +10,95 @@ import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
 import timezone from "dayjs/plugin/timezone";
 import localizedFormat from "dayjs/plugin/localizedFormat";
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Button from '@mui/material/Button';
 import AddIcon from '@mui/icons-material/Add';
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardHeader from "@mui/material/CardHeader";
+import PlaceIcon from "@mui/icons-material/Place";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 dayjs.extend(localizedFormat);
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
 export function ItineraryRight() {
+  const [open, setOpen] = useState(false);
+  const [name, setName] = useState();
+  const [description, setDescription] = useState();
+  const [cost, setCost] = useState();
+  const [location, setLocation] = useState();
+  const [chatResponse, setResponse] = useState();
+  const [date, setDate] = useState(dayjs().toDate());
+  const [time, setTime] = useState(dayjs().toDate());
+  const [errors, setErrors] = useState({ name: "", date: "", time: "" });
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setErrors({ name: "", date: "", time: "" });
+  };
+
+  const handleSave = () => {
+    // if (errors.name || errors.date || errors.time) {
+    //   alert("Please enter valid inputs before saving.");
+    //   return;
+    // }
+
+    // // Create a new event object with updated values
+    // const newEvent = {
+    //   chatResponse,
+    //   cost,
+    //   description,
+    //   endTime: dayjs(date).format("YYYY-MM-DDTHH:mm:ss.SSSZ"),
+    //   startTime: dayjs(date).format("YYYY-MM-DDTHH:mm:ss.SSSZ"),
+    //   name,
+    //   location,
+    // };
+
+    // // Flatten all events and replace the old event with the new event
+    // const allEvents = itinerary.schedule.flatMap((day) => {
+    //   return day.events.map((e) => (e.name === event.name ? newEvent : e));
+    // });
+
+    // // Sort all events by startTime
+    // allEvents.sort((a, b) => dayjs(a.startTime).diff(dayjs(b.startTime)));
+
+    // // Create a map of dates to events
+    // const dateToEventsMap = allEvents.reduce((map, event) => {
+    //   const eventDate = dayjs(event.startTime).format("YYYY-MM-DD");
+    //   if (!map[eventDate]) {
+    //     map[eventDate] = [];
+    //   }
+    //   map[eventDate].push(event);
+    //   return map;
+    // }, {});
+
+    // // Reconstruct the schedule array
+    // const newSchedule = Object.entries(dateToEventsMap).map(
+    //   ([date, events], index) => ({
+    //     day: index + 1,
+    //     date: dayjs(date).format("YYYY-MM-DDTHH:mm:ss.SSSZ"),
+    //     events,
+    //   }),
+    // );
+
+    // setItinerary({ ...itinerary, schedule: newSchedule });
+    // setOpen(false);
+  };
+
   const [itinerary, setItinerary] = useLocalStorage(
     "dailyItinerary",
     mockTravel_Itinerary1,
@@ -91,13 +172,57 @@ export function ItineraryRight() {
               setItinerary={setItinerary}
             />
             <div style={{ position: 'fixed', bottom: '20px', right: '50px' }}>
-                <Button variant="contained" endIcon={<AddIcon />}>
+                <Button variant="contained" endIcon={<AddIcon />} onClick={handleClickOpen}>
                   ADD NEW LOCATION
                 </Button>
               </div>
           </Grid>
         </Grid>
       </Background>
+
+      <Box display="flex" justifyContent="stretch" width="100%">
+      <Dialog open={open} onClose={handleClose} maxWidth="xs">
+        <DialogTitle>Add New Itinerary Item</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            label="Name"
+            type="text"
+            fullWidth
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            error={Boolean(errors.name)}
+            helperText={errors.name}
+          />
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <Box width={"100%"} mt={2}>
+              <DatePicker
+                label="Date"
+                value={dayjs(date)}
+                onChange={(newDate) => setDate(newDate.toDate())}
+                error={Boolean(errors.date)}
+                helperText={errors.date}
+              />
+            </Box>
+            {/* <Box width={"100%"} mt={2}>
+              <TimePicker
+                label="Time"
+                // Removing localisation for now.
+                value={time.tz(specificTimezone)}
+                onChange={(newTime) => setTime(newTime)}
+                error={Boolean(errors.time)}
+                helperText={errors.time}
+              />
+            </Box> */}
+          </LocalizationProvider>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleSave}>Save</Button>
+        </DialogActions>
+      </Dialog>
+      </Box>
     </div>
   );
 }
