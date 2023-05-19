0b09e41a62ab2acc3a12e5d8d6c38ff94fa2159c
cooi0004 <cooi0004@student.monash.edu>
2023-05-19 17:31:22 +1000

    fix: fix gpt return date format

--- server/src/services/OpenAIChat.service.ts
+++ server/src/services/OpenAIChat.service.ts
@@ -42,10 +42,11 @@ export async function sendOpenAIChat({ prompt, travelItinerary, chatHistory }: C
     const returnMessage: ChatCompletionRequestMessage = {
         role: "system",
         content: "help the user build an itinerary and return the updated itinerary, the itinerary should be in the format {startDate: date, endDate: date, schedule: [dailtItinerary]}. " +
-            "Daily itinerary should be in the format {day: int, date: date, activities: [activity]}. Activity should be in the format {name: str, startTime: time, endTime: time, location: str, description: str, cost: number]}" + "provide a descriptive description for the activity" +
+            "Daily itinerary should be in the format {day: int, date: date, activities: [activity]}. Activity should be in the format {name: str, startTime: date, endTime: date, location: str, description: str, cost: number]}" + "provide a descriptive description for the activity" +
             "the current itinerary is passed in as a json oject and its started as a empty itinerary" +
-            "strictly follow itinerary structrure." + "example itinerary: {startDate: 2021-07-01, endDate: 2021-07-05, schedule: [{day: 1, date: 2021-07-01, activities: [{name: 'Tokyo Tower', startTime: '10:00', endTime: '11:00', location: 'Tokyo', description: 'A tower in Tokyo', cost: 10}]}]}" +
-            "if you want to suggest an itinineray always return the itinerary following the structre and format provided above"
+            "strictly follow itinerary structrure." + "example{startDate: 2021-07-01, endDate: 2021-07-05, schedule: [{day: 1, date: 2021-07-01, activities: [{name: 'Tokyo Tower', startTime: '2023-05 - 28T10: 00: 00.000Z', endTime: '2023-05 - 28T20: 00: 00.000Z', location: 'Tokyo', description: 'A tower in Tokyo', cost: 10}]}]}" +
+            "if you want to suggest an itinineray always return the itinerary following the structre and format provided above" +
+            "along with the itinerary provide a short response to the user"
     }
 
     messages.push(itineraryContextMessage)
