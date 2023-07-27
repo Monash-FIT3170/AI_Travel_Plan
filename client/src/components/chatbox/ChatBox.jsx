809f2ec73b5fa3126195b595a6a2eeaa444b424e
Yu Xuan Yio <yioyuxuan@gmail.com>
2023-07-27 14:21:28 +1000

    fix: edited axios.get to post

--- client/src/components/chatbox/ChatBox.jsx
+++ client/src/components/chatbox/ChatBox.jsx
@@ -132,7 +132,7 @@ export default function Chatbox({
       setOutboxValue("Loading...");
       // const response = await axios.get('http://localhost:4000/api/chatMessage',
       // )
-      const response = await axios.get(
+      const response = await axios.post(
         "http://localhost:4000/api/chatMessage",
         {
           prompt: newMessage,
