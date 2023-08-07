03b827acf587debe86e726d8d1b6026c0d732e1f
Minhua Zhou <mzho0035@student.monash.edu>
2023-08-07 22:58:45 +1000

    wip: emergency contact API integrated, coutry is static

--- client/src/components/itinerary/EmergCardView.jsx
+++ client/src/components/itinerary/EmergCardView.jsx
@@ -1,49 +1,66 @@
 import React, { useState, useEffect } from "react";
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 import CardHeader from "@mui/material/CardHeader";
-import ContactEmergencyIcon from '@mui/icons-material/ContactEmergency';
-import { List, ListItem } from '@mui/material';
+import ContactEmergencyIcon from "@mui/icons-material/ContactEmergency";
+import { List, ListItem } from "@mui/material";
+import axios from "axios";
 
-  
-  export default function EmergCardView(emerg, itinerary) {
-    // const [country, setCountry] = useState(itinerary.country);
+export default function EmergCardView() {
+  const [emergencyData, setEmergencyData] = useState({
+    country: {},
+    ambulanceNumber: "",
+    policeNumber: "",
+    fireNumber: "",
+    universalNumber: "",
+  });
 
-    return (
-      <Card sx={{ minWidth: 275 }}>
+  useEffect(() => {
+    // Replace 'YOUR_API_URL' with the actual URL of your backend API
+    axios
+      .get("http://localhost:4000/api/emergencyContact?country=AU")
+      .then((response) => {
+        const data = response.data.detail; // Assuming your API response is an object with emergency details
+        console.log(data);
+        setEmergencyData(data);
+        console.log(emergencyData);
+      })
+      .catch((error) => {
+        console.error("Error fetching emergency data:", error);
+      });
+  }, [emergencyData]);
 
-        <CardHeader
-          avatar={<ContactEmergencyIcon />}
-          title={"Emergency Contacts"}
-        //   subheader={
-        //     "DURATION: " +
-        //     ((new Date(event.endTime) - new Date(event.startTime)) / (1000 * 60 * 60)) +
-        //     " HRS"}
+  const isUniversalEmergencyNumber = emergencyData.universalNumber === "";
 
-        // TODO: Make this subheader name Dynamic
-        subheader={"Services in Bali"}
-        // subheader={"Services in " + country.name}
-
-        />
-
-        <CardContent>
-
-          <Typography variant="body2">
-            {/* TODO: Make this dynamic */}
-            {/* country.description */}
-            <List>
-            <ListItem>Main Emergency: 112</ListItem>
-            <ListItem>Ambulance: 118</ListItem>
-            <ListItem>Police: 110</ListItem>
-            <ListItem>Fire Dpt: 113</ListItem>
-            <ListItem>Search & Rescue: 111, 115, 151</ListItem>
-            </List>
-          </Typography>
-        </CardContent>
-      </Card>
-    );
-  }
+  return (
+    <Card sx={{ minWidth: 275 }}>
+      <CardHeader
+        avatar={<ContactEmergencyIcon />}
+        title={"Emergency Contacts"}
+        subheader={`Services in ${emergencyData.country.countryName}`} // Update this subheader as needed
+      />
+      <CardContent>
+        <Typography variant="body2">
+          <List>
+            {isUniversalEmergencyNumber && (
+              <>
+                <ListItem>Ambulance: {emergencyData.ambulanceNumber}</ListItem>
+                <ListItem>Police: {emergencyData.policeNumber}</ListItem>
+                <ListItem>Fire Dpt: {emergencyData.fireNumber}</ListItem>
+              </>
+            )}
+            {!isUniversalEmergencyNumber && (
+              <ListItem>
+                Main Emergency: {emergencyData.universalNumber}
+              </ListItem>
+            )}
+          </List>
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
--- server/src/services/EmergencyContact.service.ts
+++ server/src/services/EmergencyContact.service.ts
@@ -26,7 +26,7 @@ export async function getEmergencyContact(
       emergencyContact.universalNumber =
         "Your Data seems to be lost on the way";
     }
-    else if (response.status != 200) {
+    else if (response.status != 200 && response.status != 201) {
       emergencyContact.universalNumber =
         "Error occured while fetching";
     } else {
@@ -38,7 +38,10 @@ export async function getEmergencyContact(
       emergencyContact.fireNumber = response.data.data.fire.all[0];
       emergencyContact.policeNumber = response.data.data.police.all[0];
       emergencyContact.ambulanceNumber = response.data.data.ambulance.all[0];
-      emergencyContact.universalNumber = response.data.data.dispatch.all[0];
+      emergencyContact.universalNumber = response.data.data.dispatch.all == null ? response.data.data.dispatch.fixed[0] : response.data.data.dispatch.all[0];
+      if (response.data.data.member_112 == true && emergencyContact.universalNumber == "") {
+        emergencyContact.universalNumber = "112";
+      }
     }
   } catch (error) {
     console.log(error);
