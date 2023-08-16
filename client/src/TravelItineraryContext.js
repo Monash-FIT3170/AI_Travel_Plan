8a0f949e398915156503cf627c18055316567ae6
cooi0004 <cooi0004@student.monash.edu>
2023-08-16 16:59:04 +1000

    refactor: move sort event and add new event into one single context file

--- client/src/TravelItineraryContext.js
+++ client/src/TravelItineraryContext.js
@@ -1,4 +1,6 @@
 import {createContext, useContext, useReducer} from "react";
+import dayjs from "dayjs";
+
 const key = "travelItinerary";
 const storedValue = localStorage.getItem(key);
 const initialState = storedValue
@@ -8,10 +10,10 @@ const TravelItineraryContext = createContext(initialState);
 const TravelItineraryDispatchContext = createContext();
 
 export function TravelItineraryProvider({children}) {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [itinerary, dispatch] = useReducer(reducer, initialState);
 
   return (
-    <TravelItineraryContext.Provider value={state}>
+    <TravelItineraryContext.Provider value={itinerary}>
       <TravelItineraryDispatchContext.Provider value={dispatch}>
         {children}
       </TravelItineraryDispatchContext.Provider>
@@ -29,10 +31,12 @@ export function useTravelItineraryDispatch() {
 
 function reducer(state, action) {
   localStorage.setItem(key, JSON.stringify(state));
+  console.log(action);
   switch (action.type) {
-    case "update":
-      console.log(action.payload);
-      return {...state, ...action.payload};
+    case "updateTravelItinerary":
+      return sortEvents(action.payload);
+    case "insertNewEvent":
+      return insertEvent(action.payload, state);
     default:
       throw new Error(`Unknown action: ${action.type}`);
   }
@@ -40,85 +44,60 @@ function reducer(state, action) {
 
 // NOTE: Reconstructing because the timezone in the mock data is not the same as the timezone in the browser.
 // TODO: When prompting gpt, provide user's timezone such that GPT returns event times in the user's timezone.
-// const reformItinerary = (itinerary) => {
-//   // Flatten all events
-//   const allEvents = itinerary.schedule.flatMap(
-//     (dailyItinerary) => dailyItinerary.activities
-//   );
+const sortEvents = (itinerary) => {
+  // Flatten all events
+  const allEvents = itinerary.schedule.flatMap(
+    (dailyItinerary) => dailyItinerary.activities
+  );
 
-//   // Sort all events by startTime
-//   allEvents.sort((a, b) => new Date(a.startTime) - new Date(b.startTime));
+  // Sort all events by startTime
+  allEvents.sort((a, b) => new Date(a.startTime) - new Date(b.startTime));
 
-//   // Create a map of dates to events
-//   const dateToEventsMap = allEvents.reduce((map, event) => {
-//     const eventDate = dayjs(event.startTime).format("YYYY-MM-DD");
-//     if (!map[eventDate]) {
-//       map[eventDate] = [];
-//     }
-//     map[eventDate].push(event);
-//     return map;
-//   }, {});
+  // Create a map of dates to events
+  const dateToEventsMap = allEvents.reduce((map, event) => {
+    const eventDate = dayjs(event.startTime).format("YYYY-MM-DD");
+    if (!map[eventDate]) {
+      map[eventDate] = [];
+    }
+    map[eventDate].push(event);
+    return map;
+  }, {});
 
-//   // Reconstruct the schedule array
-//   const newSchedule = Object.entries(dateToEventsMap).map(
-//     ([date, activities], index) => ({
-//       day: index + 1,
-//       date: new Date(date),
-//       activities,
-//     })
-//   );
+  // Reconstruct the schedule array
+  const newSchedule = Object.entries(dateToEventsMap).map(
+    ([date, activities], index) => ({
+      day: index + 1,
+      date: new Date(date),
+      activities,
+    })
+  );
 
-//   // Return a new itinerary object
-//   return {...itinerary, schedule: newSchedule};
-// };
+  // Return a new itinerary object
+  return {...itinerary, schedule: newSchedule};
+};
 
-//   const reformItinerary = (itinerary) => {
-//     // Flatten all events
-//     const allEvents = itinerary.schedule.flatMap(
-//       (dailyItinerary) => dailyItinerary.activities
-//     );
+function insertEvent(newEvent, itinerary) {
+  //insert into schedule if there is same event
+  const allEvents = itinerary.schedule.flatMap((day) => {
+    return day.activities.map((e) => (e.name === newEvent.name ? newEvent : e));
+  });
 
-//     // Sort all events by startTime
-//     allEvents.sort((a, b) => new Date(a.startTime) - new Date(b.startTime));
+  return sortEvents(allEvents);
+}
 
-//     // Create a map of dates to events
-//     const dateToEventsMap = allEvents.reduce((map, event) => {
-//       const eventDate = dayjs(event.startTime).format("YYYY-MM-DD");
-//       if (!map[eventDate]) {
-//         map[eventDate] = [];
-//       }
-//       map[eventDate].push(event);
-//       return map;
-//     }, {});
+// const refactorItinerary = (itinerary) => {
+//   const format = "YYYY-MM-DDTHH:mm:ss.SSSZ";
 
-//     // Reconstruct the schedule array
-//     const newSchedule = Object.entries(dateToEventsMap).map(
-//       ([date, activities], index) => ({
-//         day: index + 1,
-//         date: new Date(date),
-//         activities,
-//       })
-//     );
+//   if (itinerary.schedule.length > 0) {
+//     const newItinerary = reformItinerary(itinerary);
 
-//     // Return a new itinerary object
-//     return {...itinerary, schedule: newSchedule};
-//   };
-
-//   const refactorItinerary = (itinerary) => {
-//     const format = "YYYY-MM-DDTHH:mm:ss.SSSZ";
-
-//     if (itinerary.schedule.length > 0) {
-//       const newItinerary = reformItinerary(itinerary);
-
-//       newItinerary.schedule.forEach((dailyItinerary) => {
-//         console.log(dailyItinerary);
-//         dailyItinerary.date = dayjs(dailyItinerary.date).format(format);
-//         dailyItinerary.activities.forEach((event) => {
-//           event.startTime = dayjs(event.startTime).format(format);
-//           event.endTime = dayjs(event.endTime).format(format);
-//         });
+//     newItinerary.schedule.forEach((dailyItinerary) => {
+//       console.log(dailyItinerary);
+//       dailyItinerary.date = dayjs(dailyItinerary.date).format(format);
+//       dailyItinerary.activities.forEach((event) => {
+//         event.startTime = dayjs(event.startTime).format(format);
+//         event.endTime = dayjs(event.endTime).format(format);
 //       });
-
-//       setItinerary(newItinerary);
-//     }
-//   };
+//     });
+//   }
+// };
