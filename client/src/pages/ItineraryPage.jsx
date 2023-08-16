12571dd0d2a5f7d8957be5eb172bbe0883a6e9aa
cooi0004 <cooi0004@student.monash.edu>
2023-08-16 15:49:51 +1000

    refactor: rename pages

---
+++ client/src/pages/ItineraryPage.jsx
@@ -0,0 +1,10 @@
+import Background from "../components/background/Background";
+import BackgroundImage from "../components/background/BackgroundImage";
+
+export function ItineraryPage() {
+  return (
+    <BackgroundImage>
+      <Background></Background>;
+    </BackgroundImage>
+  );
+}
