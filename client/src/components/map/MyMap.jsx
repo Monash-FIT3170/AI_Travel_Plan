813ee66cf95f5e7f773114b9db2d69ac848ab473
arenk <arenk20@gmail.com>
2023-08-16 13:24:23 +1000

    feat: safeguard in case mapbox api not detected.

--- client/src/components/map/MyMap.jsx
+++ client/src/components/map/MyMap.jsx
@@ -3,10 +3,15 @@ import React, { useRef, useEffect, useState } from "react";
 import "mapbox-gl/dist/mapbox-gl.css";
 import Map, { Marker } from "react-map-gl";
 
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+
 const ACCESS_TOKEN = process.env.REACT_APP_MAPBOX_API_KEY;
 
 /**
  * React component for the map on itinerary page
+ * remember to do an npm install in the client folder to download all map packages.
  * requires a mapbox access token. Can be generated here: https://docs.mapbox.com/help/getting-started/access-tokens/
  *  create a .env file in the client folder and add your mapbox token there in the form: 
  *  REACT_APP_MAPBOX_API_KEY="your api key"
@@ -15,6 +20,19 @@ const ACCESS_TOKEN = process.env.REACT_APP_MAPBOX_API_KEY;
 const markerIconUrl = "../assets/marker_blue.png";
 
 export default function MyMap() {
+  // safeguard in case there is no mapbox api token
+  if (!ACCESS_TOKEN) {
+    return (
+      <React.Fragment>
+        <CssBaseline />
+        <Container width="100%">
+          <Box sx={{ bgcolor: '#cfe8fc', height: 'calc(100vh - 200px)' }} >
+            <Typography>No Mapbox API key was detected. Follow instructions in components/map/MyMap.jsx comments to load map.</Typography>
+            </Box>
+        </Container>
+      </React.Fragment>
+    );
+  }
   return (
     <div>
       <Map
