8360bba7f63b0ba91b211736710aba3cd4d16a77
Ctha0006 <ctha0006@student.monash.edu>
2023-08-21 23:03:44 +1000

    fix: ignore buttons for pdf export, change button icon and adjust image height on page

--- client/src/components/itinerary/PDFexport.jsx
+++ client/src/components/itinerary/PDFexport.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import html2canvas from "html2canvas";
 import jsPDF from "jspdf";
-import AddIcon from "@mui/icons-material/Add";
+import DownloadIcon from '@mui/icons-material/Download';
 import { Button } from "@mui/material";
 
 const PdfDownload = ({rootElementId , downloadFileName}) => {
@@ -18,7 +18,8 @@ const PdfDownload = ({rootElementId , downloadFileName}) => {
             input.scrollTo(0, 0);
             html2canvas(input, {
                 allowTaint: true, scale: '1', backgroundColor: '#87CEEB',
-                height: input.scrollHeight, windowHeight: input.scrollHeight}).then(canvas => {
+                height: input.scrollHeight, windowHeight: input.scrollHeight
+            }).then(canvas => {
                 const imgData = canvas.toDataURL('image/png');
                 const imgWidth = 210;
                 const pageHeight = 290;
@@ -26,21 +27,21 @@ const PdfDownload = ({rootElementId , downloadFileName}) => {
                 let heightLeft = imgHeight;
                 const doc = new jsPDF('pt', 'mm');
                 let position = 0;
-                doc.addImage(imgData, 'PNG', 0, 0, imgWidth, imgHeight + 25);
+                doc.addImage(imgData, 'PNG', 0, 0, imgWidth, imgHeight);
                 heightLeft -= pageHeight;
                 while (heightLeft >= 0) {
                     position = heightLeft - imgHeight;
                     doc.addPage();
-                    doc.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight + 25);
+                    doc.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
                     heightLeft -= pageHeight;
                 }
-                doc.save('download.pdf');
+                doc.save(`${downloadFileName}.pdf`);
                 setLoader(false);
             });
         }, 1000);
     };
 
-    return <Button variant="contained" endIcon={<AddIcon />} 
+    return <Button variant="contained" endIcon={<DownloadIcon />} 
 		onClick={downloadPdfDocument}>Download Itinerary</Button>
 }
 
