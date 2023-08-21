aad3fd7484fad6284ba6132f6d022b337a84a9d2
github-devireddyprasanth22 <dpkrdrkr@gmail.com>
2023-08-21 22:43:40 +1000

    Adding marker and label on map

--- client/src/components/map/MyMap.jsx
+++ client/src/components/map/MyMap.jsx
@@ -1,11 +1,13 @@
 import { Typography } from "@mui/material";
 import React, { useRef, useEffect, useState } from "react";
 import "mapbox-gl/dist/mapbox-gl.css";
-import Map, { Marker } from "react-map-gl";
+import Map, { Marker, Popup } from "react-map-gl";
+import markerIconPng from "leaflet/dist/images/marker-icon.png";
+import { Icon } from "leaflet";
 
-import CssBaseline from '@mui/material/CssBaseline';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
 
 const ACCESS_TOKEN = process.env.REACT_APP_MAPBOX_API_KEY;
 
@@ -13,11 +15,10 @@ const ACCESS_TOKEN = process.env.REACT_APP_MAPBOX_API_KEY;
  * React component for the map on itinerary page
  * remember to do an npm install in the client folder to download all map packages.
  * requires a mapbox access token. Can be generated here: https://docs.mapbox.com/help/getting-started/access-tokens/
- *  create a .env file in the client folder and add your mapbox token there in the form: 
+ *  create a .env file in the client folder and add your mapbox token there in the form:
  *  REACT_APP_MAPBOX_API_KEY="your api key"
  * @returns jsx for the map
  */
-const markerIconUrl = "../assets/marker_blue.png";
 
 export default function MyMap() {
   // safeguard in case there is no mapbox api token
@@ -26,9 +27,12 @@ export default function MyMap() {
       <React.Fragment>
         <CssBaseline />
         <Container width="100%">
-          <Box sx={{ bgcolor: '#cfe8fc', height: 'calc(100vh - 200px)' }} >
-            <Typography>No Mapbox API key was detected. Follow instructions in components/map/MyMap.jsx comments to load map.</Typography>
-            </Box>
+          <Box sx={{ bgcolor: "#cfe8fc", height: "calc(100vh - 200px)" }}>
+            <Typography>
+              No Mapbox API key was detected. Follow instructions in
+              components/map/MyMap.jsx comments to load map.
+            </Typography>
+          </Box>
         </Container>
       </React.Fragment>
     );
@@ -46,40 +50,23 @@ export default function MyMap() {
         mapStyle="mapbox://styles/mapbox/streets-v9"
         attributionControl={false}
       >
+        <Popup longitude={144.946457} latitude={-37.840935}>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
         {/* Add the marker with label here */}
         <Marker
+          icon={
+            new Icon({
+              iconUrl: markerIconPng,
+              iconSize: [25, 41],
+              iconAnchor: [12, 41],
+            })
+          }
           longitude={144.946457}
           latitude={-37.840935}
           offsetLeft={-20}
           offsetTop={-10}
-        >
-          {/* for marker customization*/}
-          <div
-            style={{
-              width: "40px",
-              height: "40px",
-              borderRadius: "50%",
-              backgroundImage: `url(${markerIconUrl})`,
-              backgroundSize: "cover",
-            }}
-          >
-            {/* <img src="../../assets/marker_blue.png" /> */}
-          </div>
-          {/* Add the label */}
-          <Typography
-            variant="caption"
-            style={{
-              position: "relative",
-              top: "-20px",
-              left: "-10px",
-              background: "white",
-              padding: "4px 8px",
-              borderRadius: "4px",
-            }}
-          >
-            Marker
-          </Typography>
-        </Marker>
+        ></Marker>
       </Map>
     </div>
   );
