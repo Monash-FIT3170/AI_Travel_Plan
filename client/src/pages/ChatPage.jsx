c1c05fde32cbb785730599e15e14376b57deff95
Yu Xuan Yio <yioyuxuan@gmail.com>
2023-08-25 20:29:36 +1000

    feat: readded animation from landing to chat

--- client/src/pages/ChatPage.jsx
+++ client/src/pages/ChatPage.jsx
@@ -1,12 +1,14 @@
 import Background from "../components/background/Background";
 import BackgroundImage from "../components/background/BackgroundImage";
-import {ItineraryTimeLine} from "../components/itinerary/ItineraryTimeLine";
+import { ItineraryTimeLine } from "../components/itinerary/ItineraryTimeLine";
 import Grid from "@mui/material/Grid";
 import ChatBox from "../components/chatbox/ChatBox";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Button from "@mui/material/Button";
+import { motion } from "framer-motion";
+import { useLocation } from "react-router-dom";
 
-import {AddNewLocationFAB} from "../components/itinerary/AddNewLocationFAB";
+import { AddNewLocationFAB } from "../components/itinerary/AddNewLocationFAB";
 import {
   TravelItineraryProvider,
   useTravelItinerary,
@@ -14,6 +16,8 @@ import {
 
 export function ChatPage() {
   const [chatBoxKey, setChatBoxKey] = useState(1); // Add a state for key
+  const locationHistory = useLocation();
+  const fromHomePage = locationHistory.state?.fromHomePage;
 
   const clearChat = () => {
     // Remove chat history from local storage
@@ -21,31 +25,40 @@ export function ChatPage() {
     setChatBoxKey((prevKey) => prevKey + 1); // Increment the key to force remount
   };
   return (
-    <div style={{position: "relative"}}>
-      <BackgroundImage />
-      <Background>
-        <TravelItineraryProvider>
-          <Grid container>
-            <Grid item xs={6}>
-              <Button
-                onClick={clearChat}
-                variant="contained"
-                color="primary"
-                style={{marginBottom: "10px"}}
-              >
-                Clear
-              </Button>
-              <ChatBox key={chatBoxKey}></ChatBox>
+    <motion.div
+      initial={{ y: fromHomePage ? "100vh" : 0 }}
+      animate={{ y: 0 }}
+      exit={{ y: "100vh" }}
+      transition={{ duration: 0.5 }}
+    >
+      <div style={{ position: "relative" }}>
+        <BackgroundImage />
+        <Background>
+          <TravelItineraryProvider>
+            <Grid container>
+              <Grid item xs={6}>
+                <Button
+                  onClick={clearChat}
+                  variant="contained"
+                  color="primary"
+                  style={{ marginBottom: "10px" }}
+                >
+                  Clear
+                </Button>
+                <ChatBox key={chatBoxKey}></ChatBox>
+              </Grid>
+              <Grid item xs={6} style={{ height: "100vh", overflowY: "auto" }}>
+                <ItineraryTimeLine />
+                <div
+                  style={{ position: "fixed", bottom: "20px", right: "50px" }}
+                >
+                  <AddNewLocationFAB></AddNewLocationFAB>
+                </div>
+              </Grid>
             </Grid>
-            <Grid item xs={6} style={{height: "100vh", overflowY: "auto"}}>
-              <ItineraryTimeLine />
-              <div style={{position: "fixed", bottom: "20px", right: "50px"}}>
-                <AddNewLocationFAB></AddNewLocationFAB>
-              </div>
-            </Grid>
-          </Grid>
-        </TravelItineraryProvider>
-      </Background>
-    </div>
+          </TravelItineraryProvider>
+        </Background>
+      </div>
+    </motion.div>
   );
 }
