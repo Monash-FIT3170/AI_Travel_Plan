4837cfd4e4c0c60678af11963663465b2597e8b7
cooi0004 <cooi0004@student.monash.edu>
2023-08-26 23:12:16 +1000

    fix: sorting itinerary and adding new itinerary

--- client/src/TravelItineraryContext.js
+++ client/src/TravelItineraryContext.js
@@ -1,11 +1,13 @@
 import {createContext, useContext, useReducer} from "react";
 import dayjs from "dayjs";
+import {all} from "axios";
 
 const key = "travelItinerary";
 const storedValue = localStorage.getItem(key);
+console.log(storedValue);
 const initialState = storedValue
   ? JSON.parse(storedValue)
-  : {startDate: null, endDate: null, country: null, schedule: []};
+  : {startDate: null, endDate: null, country: null, schedule: null};
 const TravelItineraryContext = createContext(initialState);
 const TravelItineraryDispatchContext = createContext();
 
@@ -30,67 +32,82 @@ export function useTravelItineraryDispatch() {
 }
 
 function reducer(state, action) {
-  localStorage.setItem(key, JSON.stringify(state));
-  console.log(action);
-  switch (action.type) {
-    case "updateTravelItinerary":
-      return action.payload.schedule
-        ? sortEvents(action.payload)
-        : action.payload;
-    case "insertNewEvent":
-      console.log(action.payload.activities[0]);
-      console.log(state);
-      if (state.schedule) {
-        return insertEvent(action.payload.activities[0], state);
-      } else {
-        return {...state, schedule: [action.payload]};
-      }
-    default:
-      throw new Error(`Unknown action: ${action.type}`);
+  let newState;
+  if (action.type == "updateTravelItinerary") {
+    if (action.payload.schedule) {
+      newState = sortEvents(action.payload);
+    }
+    newState = {...action.payload};
+  } else if (action.type == "insertNewEvent") {
+    if (state.schedule) {
+      newState = insertEvent(action.payload.activities[0], state);
+    } else {
+      newState = {...state, schedule: [action.payload]};
+    }
+  } else {
+    throw new Error(`Unknown action: ${action.type}`);
   }
+  localStorage.setItem(key, JSON.stringify(newState));
+  return newState;
 }
 
 // NOTE: Reconstructing because the timezone in the mock data is not the same as the timezone in the browser.
 // TODO: When prompting gpt, provide user's timezone such that GPT returns event times in the user's timezone.
 const sortEvents = (itinerary) => {
   console.log(itinerary);
+
   // Flatten all events
-  const allEvents = itinerary.schedule;
+  const allEvents = itinerary.schedule
+    ?.flatMap((day) => day.activities)
+    .map((activity) => activity);
 
-  console.log(allEvents);
-
-  // Sort all events by startTime
-  allEvents.sort((a, b) => new Date(a.startTime) - new Date(b.startTime));
-
-  // Create a map of dates to events
-  const dateToEventsMap = allEvents.reduce((map, event) => {
-    const eventDate = dayjs(event.startTime).format("YYYY-MM-DD");
-    if (!map[eventDate]) {
-      map[eventDate] = [];
-    }
-    map[eventDate].push(event);
-    return map;
-  }, {});
-
-  // Reconstruct the schedule array
-  const newSchedule = Object.entries(dateToEventsMap).map(
-    ([date, activities], index) => ({
-      day: index + 1,
-      date: new Date(date),
-      activities,
-    })
+  const allActivitySorted = allEvents?.sort(
+    (a, b) => new Date(a.startTime) - new Date(b.startTime)
   );
+  console.log(allEvents);
+  console.log(allActivitySorted);
+
+  const scheduleStart = [
+    {
+      day: 1,
+      date: dayjs(allActivitySorted[0].startTime).format("YYYY-MM-DD"),
+      activities: [],
+    },
+  ];
+
+  const newSchedule = allEvents.reduce((acc, cur) => {
+    const latestSchedule = acc[acc.length - 1];
+    if (latestSchedule.date === dayjs(cur.startTime).format("YYYY-MM-DD")) {
+      return [
+        ...acc.slice(0, -1),
+        {...latestSchedule, activities: [...latestSchedule.activities, cur]},
+      ];
+    } else {
+      return [
+        ...acc,
+        {
+          day: latestSchedule.day + 1,
+          date: dayjs(cur.startTime).format("YYYY-MM-DD"),
+          activities: [cur],
+        },
+      ];
+    }
+  }, scheduleStart);
 
   // Return a new itinerary object
   return {...itinerary, schedule: newSchedule};
 };
 
 function insertEvent(newEvent, itinerary) {
+  const latestSchedule = itinerary.schedule[itinerary.schedule.length - 1];
   //insert into schedule if there is same event
-  itinerary.schedule[0].activities.push(newEvent);
-  const allEvents = itinerary.schedule.flatMap((day) => {
-    return day.activities.map((e) => (e.name === newEvent.name ? newEvent : e));
+  const updatedLatesetSchedule = latestSchedule.activities.concat(newEvent);
+  console.log(updatedLatesetSchedule);
+  return sortEvents({
+    ...itinerary,
+    schedule: [
+      ...itinerary.schedule.slice(0, -1),
+      {...latestSchedule, activities: updatedLatesetSchedule},
+    ],
   });
-  console.log(allEvents);
-  return sortEvents({...itinerary, schedule: allEvents});
 }
