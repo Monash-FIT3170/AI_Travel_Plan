592acc1de94dbdfae6b099b2dc204dd880bb9eee
cooi0004 <cooi0004@student.monash.edu>
2023-10-01 23:11:55 +1100

    test: added test cases for openai services

---
+++ server/test/OpenAIChat-service.test.ts
@@ -0,0 +1,71 @@
+import request from 'supertest';
+import express from 'express';
+import { postMessageRequest } from '../src/controllers/ChatMessageController';
+const app = express();
+app.use(express.json());
+
+// Mock the sendOpenAIChat and parseResponse functions
+jest.mock('../src/services/OpenAIChat.service', () => ({
+    sendOpenAIChat: jest.fn(),
+}));
+jest.mock('../src/controllers/ChatMessageController', () => ({
+    parseResponse: jest.fn(),
+}));
+
+describe('postMessageRequest', () => {
+    it('should respond with a parsed response', async () => {
+        // Mock the sendOpenAIChat and parseResponse functions
+        const mockResponse = [{
+            "message": {
+                "role": "assistant",
+                "content": {
+                    "chatResponse": "Test chat response",
+                    "needConfirmation": false
+                }
+            },
+            "finish_reason": "stop"
+        }
+        ];
+        const mockParsedResponse = { chatResponse: 'Test chat response', needConfirmation: false };
+
+        // Mock the sendOpenAIChat function to return mockResponse
+        (require('../src/services/OpenAIChat.service').sendOpenAIChat as jest.Mock).mockResolvedValue(mockResponse);
+
+        // Mock the parseResponse function to return mockParsedResponse
+        (require('../src/controllers/ChatMessageController').parseResponse as jest.Mock).mockReturnValue(mockParsedResponse);
+
+        const requestBody = {
+            prompt: 'Sample prompt',
+            travelItinerary: {
+            },
+            chatHistory: [],
+            additionalInfo: 'Additional info',
+        };
+
+        const response = await request(app)
+            .post('/api/chatMessage')
+            .send(requestBody);
+
+        expect(response.status).toBe(201);
+        // expect(response.body).toEqual(mockParsedResponse);
+    });
+
+    it('should respond with an error message if sendOpenAIChat throws an error', async () => {
+        // Mock sendOpenAIChat to throw an error
+        (require('../src/services/OpenAIChat.service').sendOpenAIChat as jest.Mock).mockRejectedValue(new Error('Test error'));
+
+        const requestBody = {
+            prompt: 'Sample prompt',
+            travelItinerary: {
+            },
+            chatHistory: [],
+            additionalInfo: 'Additional info',
+        };
+
+        const response = await request(app)
+            .post('/api/chatMessage')
+            .send(requestBody);
+
+        expect(response.status).toBe(404);
+    });
+});
